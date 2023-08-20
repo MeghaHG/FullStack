b0849e88ac6a1f83225670c1b07807e89ed3bdc4
MeghaHG <115128842+MeghaHG@users.noreply.github.com>
2023-08-20 16:45:35 +0530

    Add files via upload

---
+++ JavaScript/demo.js
@@ -0,0 +1,10 @@
+console.log("Hello JavaScript")
+console.log(20+40)
+console.log(12*2)
+console.log(20/2)
+console.log(29-3)
+console.log(12%2)
+
+document.write("Hello JS")
+
+document.write(12*2)
